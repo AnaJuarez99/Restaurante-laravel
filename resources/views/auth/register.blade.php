83401d2bb5f1400da0844205715c065cd5803836
Ana Juárez García <anajuarezz99@gmail.com>
2023-01-13 12:25:47 +0100

    2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
                                     <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nombre') }}</label>
 
                                     <div class="col-md-6">
-                                        <input id="name" type="text" class="form-control @error('Nombre') is-invalid @enderror" name="Nombre" value="{{ old('Nombre') }}" required autocomplete="Nombre" autofocus>
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
