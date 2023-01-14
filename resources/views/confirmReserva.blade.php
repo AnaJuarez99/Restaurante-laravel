e4816506add8ad939c2a155ab1e8a18697677448
Ana Juárez García <anajuarezz99@gmail.com>
2023-01-14 13:36:07 +0100

    confirm

--- resources/views/confirmReserva.blade.php
+++ resources/views/confirmReserva.blade.php
@@ -44,45 +44,47 @@
 		
 			<div >
 				<img class="contenedor2" src="./fotos/fondo.jpg" />
-		
-				<div class="container6">
-					<h1 class="r"><u>MIS RESERVAS</u></h1>
-					<table class="confirm ">
-						<thead class="resp">
-							<thead>
-								<tr>
-									<th>Fecha</th>
-									<th>Hora</th>
-									<th>Menú</th>
-									<th>Comensales</th>
-									<th>Precio Menú</th>
-									<th>Precio Total</th>
-									<th>Opciones</th>
-								</tr>
+
+
+				<div class="aa">
+					<div class="container6">
+						<h1 class="r"><u>MIS RESERVAS</u></h1>
+						<table class="confirm">
+							<thead class="resp">
+								<thead>
+									<tr>
+										<th>Fecha</th>
+										<th>Hora</th>
+										<th>Menú</th>
+										<th>Comensales</th>
+										<th>Precio Menú</th>
+										<th>Precio Total</th>
+										<th>Opciones</th>
+									</tr>
+								</thead>
 							</thead>
-						</thead>
 
-						@isset($reser)
-							@foreach ($reser as $r )
-		
-							<tr>
-								<td>{{$r->horario->fecha}}</td>
-								<td>{{$r->horario->hora}}</td>
-								<td>{{$r->menu->nombre}}</td>
-								<td>{{$r->num_personas}}</td>
-								<td>{{$r->menu->precio}}</td>
-								<td>{{$r->menu->precio*$r->num_personas}}</td>
-								<td></td>
-								
-							</tr>
-								
-							@endforeach
-						@endisset
+							@isset($reser)
+								@foreach ($reser as $r )
+			
+								<tr>
+									<td>{{$r->horario->fecha}}</td>
+									<td>{{$r->horario->hora}}</td>
+									<td>{{$r->menu->nombre}}</td>
+									<td>{{$r->num_personas}}</td>
+									<td>{{$r->menu->precio}}</td>
+									<td>{{$r->menu->precio*$r->num_personas}}</td>
+									<td></td>
+									
+								</tr>
+									
+								@endforeach
+							@endisset
 
-						
-					</table>
+							
+						</table>
+					</div>
 				</div>
-				
 			</div>
 
 	<hr>
