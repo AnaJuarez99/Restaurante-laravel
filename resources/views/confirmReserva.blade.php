bf5ae511f1977abbde9841e5fc82cda6ff56492d
Ana Juárez García <anajuarezz99@gmail.com>
2023-01-26 18:03:45 +0100

    18:03

--- resources/views/confirmReserva.blade.php
+++ resources/views/confirmReserva.blade.php
@@ -1,56 +1,17 @@
 @extends('layouts.app')
 @section("content")
-	{{-- <div >
-		<img class="contenedor2" src="./fotos/fondo.jpg" />
-
-
-		<div class="container">
-			<h1><u>MIS RESERVAS</u></h1>
-			<table class="confirm">
-				<thead class="resp">
-					<thead>
-						<tr>
-							<th>Nombre</th>
-							<th>Personas</th>
-							<th>Fecha</th>
-							<th>Hora</th>
-							<th>Factura</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach ( $savedate as $calldates)
-							
-							<tr>
-								<td>{{$calldates->nombre .''. $calldates->apellido}}</td>
-								<td>{{$calldates->persona }}</td>
-								<td>{{$calldates->fecha}}</td>
-								<td>{{$calldates->hora}}</td>
-								<td><button type="button"> <img src="./fotos/download.png" /></button></td>
-
-							</tr>
-
-						@endforeach
-
-					</tbody>
-				</thead>
-
-			</table>
-
-
-
-		</div>
-	</div> --}}
+	
 
 		
 			<div >
 				<img class="contenedor2" src="./fotos/fondo.jpg" />
 
 
-				<div class="aa">
+				
 					<div class="container6">
 						<h1 class="r"><u>MIS RESERVAS</u></h1>
-						<table class="confirm">
-							<thead class="resp">
+						<table class="table-bordered">
+							<thead>
 								<thead>
 									<tr>
 										<th>Fecha</th>
@@ -84,7 +45,7 @@
 							
 						</table>
 					</div>
-				</div>
+				
 			</div>
 
 	<hr>
